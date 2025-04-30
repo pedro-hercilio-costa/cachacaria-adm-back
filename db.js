5899a1ea604107b097db5777fc6fcc28b7cabc02
pedro-hercilio-costa <pedro_hercilio-costa@estudante.sc.senai.br>
2025-04-29 22:19:58 -0300

    Dados de Acesso Postgres

--- db.js
+++ db.js
@@ -1,1 +1,23 @@
--- Nada Ainda
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'cachacaria_adm',
+    password: '1234',
+    port: 5432,
+    max: 20,
+    idleTimeoutMillis: 50000,
+    connectionTimeoutMillis: 5000,
+});
+
+pool.on('connect', () => {
+    console.log('Connected to the database');
+});
+
+pool.on('error', (err, client) => {
+    console.error('Unexpected error on PostgreSQL client', err);
+    process.exit(-1);
+});
+
+module.exports = pool;

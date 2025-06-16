a0f2ed07a16072dde7b0322b189a88e639fd5b53
Pietro <pietrotoldo1@gmail.com>
2025-06-15 21:22:06 -0300

    feat: conecta backend à Railway e configura .env

--- db.js
+++ db.js
@@ -1,23 +1,24 @@
+require('dotenv').config();
+console.log('DATABASE_URL carregada:', process.env.DATABASE_URL);
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'cachacaria_adm',
-    password: '1234',
-    port: 5432,
-    max: 20,
-    idleTimeoutMillis: 50000,
-    connectionTimeoutMillis: 5000,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // necessário para o Railway funcionar
+  },
+  max: 20,
+  idleTimeoutMillis: 50000,
+  connectionTimeoutMillis: 5000,
 });
 
 pool.on('connect', () => {
-    console.log('Connected to the database');
+  console.log('Connected to the database');
 });
 
 pool.on('error', (err, client) => {
-    console.error('Unexpected error on PostgreSQL client', err);
-    process.exit(-1);
+  console.error('Unexpected error on PostgreSQL client', err);
+  process.exit(-1);
 });
 
 module.exports = pool;
